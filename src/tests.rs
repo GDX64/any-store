e05a28192d6fd195b89091aa0c70130f0e809a41
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-01-03 12:42:54 -0300

    using a stack

---
+++ src/tests.rs
@@ -0,0 +1,67 @@
+use crate::{
+    storage,
+    value::{self, Serializable, Something},
+};
+
+fn setup() -> storage::Table {
+    let mut store = storage::Table::new();
+    let v1 = Something::string("hello".into());
+    let k1 = Something::Int(10);
+    store.insert_at(k1.clone(), v1.clone(), 5);
+    let v2 = Something::string("world".into());
+    let k2 = Something::Int(20);
+    store.insert_at(k2.clone(), v2.clone(), 3);
+    let k3 = Something::Int(30);
+    store.insert_at(k3.clone(), v1.clone(), 5);
+
+    return store;
+}
+
+#[test]
+fn it_works() {
+    let store = setup();
+    let k1 = Something::Int(10);
+    let value = store.get(&k1).map(|r| r.get(5));
+    let v1 = Something::string("hello".into());
+    assert_eq!(value, Some(&v1));
+    let value = store.get(&Something::Int(-1));
+    assert_eq!(value, None);
+}
+
+#[test]
+fn test_rows_with() {
+    let store = setup();
+    let rows = store.rows_with(|r| {
+        return r.get(5) == &Something::string("hello".into());
+    });
+
+    assert_eq!(rows.count(), 2);
+}
+
+#[test]
+fn test_ordering() {
+    let store = setup();
+    let range = store.get_range(&Something::Int(15), &Something::Int(35));
+    assert!(range.count() > 1);
+}
+
+#[test]
+fn test_replication() {
+    let mut store = setup();
+    let ops = store.take_ops();
+    let store2 = storage::Table::from_ops(ops);
+    let h1 = store.tree_hash();
+    let h2 = store2.tree_hash();
+    assert_eq!(h1, h2);
+}
+
+#[test]
+fn serialization_test() {
+    let store = setup();
+    let row = store.get(&Something::Int(10)).unwrap();
+    let mut buffer = value::ByteBuffer::new();
+    row.serialize(&mut buffer);
+    buffer.reset();
+    let deserialized = storage::Row::deserialize(&mut buffer);
+    assert_eq!(row, &deserialized);
+}
