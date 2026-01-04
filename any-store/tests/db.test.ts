76569f2ec1437a176cd25a05459cc14e9786ebe5
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-01-04 11:30:34 -0300

    tests working

---
+++ any-store/tests/db.test.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { WDB } from "../src/WDB";
+
+describe("Database Module", () => {
+  test("should initialize the database correctly", async () => {
+    const data = fs.readFileSync(
+      "../target/wasm32-unknown-unknown/release/any_store.wasm"
+    );
+
+    const wdb = await WDB.create(data);
+    const table = wdb.createTable({
+      name: "string",
+      age: "i32",
+      height: "f64",
+    });
+    const k1 = WDB.i32(123);
+    table.insert(k1, WDB.string("Alice"), "name");
+    table.insert(k1, WDB.i32(30), "age");
+    table.insert(k1, WDB.f64(1.75), "height");
+    table.insert(WDB.i32(0), WDB.string("Bob"), "name");
+
+    const row1 = table.row(k1);
+    expect(row1.get("name")).toBe("Alice");
+    expect(row1.get("age")).toBe(30);
+    expect(row1.get("height")).toBeCloseTo(1.75);
+
+    const row2 = table.row(WDB.i32(0));
+    expect(row2.get("name")).toBe("Bob");
+  });
+});
