6a946f6a4a655ce59d96fe572db377583e86b1c0
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-03-01 20:10:28 -0300

    counter test

---
+++ any-store/browser-tests/counter.test.ts
@@ -0,0 +1,17 @@
+import { AnyStore } from "../src/AnyStore";
+import { describe, expect, test } from "vitest";
+
+describe("counter test", () => {
+  test("counter", async () => {
+    const db = await AnyStore.create();
+    const table = db.createTable("counter", { value: "i32" });
+    const row = table.row(AnyStore.i32(1));
+    db.withLock(() => {
+      row.value = 0;
+      for (let i = 0; i < 1000_000; i++) {
+        row.value += 1;
+      }
+    });
+    expect(row.value).toBe(1000_000);
+  });
+});
